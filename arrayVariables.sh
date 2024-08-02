8e9b5bef7df2a51c49b4d78aa2b3aa181dfcce2d
thefighter01 <mahmoudbubg1234@gmail.com>
2024-08-02 18:25:41 +0300

    i hope we can achieve it

--- arrayVariables.sh
+++ arrayVariables.sh
@@ -1,1 +1,28 @@
 #! /usr/bin/bash
+
+os=('ubuntu' 'windows' 'kali')
+
+os[6]="mac"
+
+unset os[2]
+
+echo "${os[@]}" # this will print the whole array
+
+echo "${os[1]}"
+
+echo "${!os[@]}" # this will print the indices of the array
+
+echo "${#os[@]}" # this will print the length of the array
+
+string="abcdefghigkl"
+
+echo "${string[@]}"
+
+echo "${string[0]}"
+
+echo "${#string[0]}"
+
+echo "${#string[@]}"
+echo "${!string[0]}"
+
+echo "${string[1]}"
--- caseExample.sh
+++ caseExample.sh
@@ -1,1 +1,27 @@
 #! /usr/bin/bash
+
+echo -e "enter some character : \c"
+
+read -r value
+
+case $value in
+[a-z])
+    echo "user enter $value which is in the range a to z"
+    ;;
+
+[A-Z])
+    echo "user enter $value which is in the range A to Z"
+    ;;
+
+[0-9])
+    echo "user enter $value which is in the range 0 to 9"
+    ;;
+
+?) # any special character not a number or a-z or A-Z but only one character
+    echo "user enter $value " ;;
+
+*)
+    echo "user enter some shit which is $value "
+    ;;
+
+esac
