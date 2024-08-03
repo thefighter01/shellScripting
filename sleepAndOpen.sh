dcc2b3e5625786a6c1ebfa73179764156eae7ce9
thefighter01 <mahmoudbubg1234@gmail.com>
2024-08-03 14:05:12 +0300

    "letsGoooo"

--- sleepAndOpen.sh
+++ sleepAndOpen.sh
@@ -1,1 +1,17 @@
 #! /usr/bin/bash
+
+n=1
+
+while [ "$n" -le 3 ]; do
+    echo "$n"
+    gnome-terminal &
+    ((n++))
+done
+
+# n=1
+
+# while (("$n" <= 3)); do
+#     echo "$n"
+#     xterm &
+#     ((++n))
+# done
--- whileLoops.sh
+++ whileLoops.sh
@@ -1,1 +1,13 @@
 #! /usr/bin/bash
+
+n=1
+
+while [ "$n" -le 10 ]; do
+    echo "$n"
+    n=$((n + 1))
+done
+
+while (("$n" < 15)); do
+    echo "$n"
+    ((n++))
+done
