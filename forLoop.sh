0848f48dc624388d3b34c9d77f051fcb89e2f73d
thefighter01 <mahmoudbubg1234@gmail.com>
2024-08-05 23:03:24 +0300

    4DayRemaining

--- forLoop.sh
+++ forLoop.sh
@@ -1,1 +1,21 @@
 #! /usr/bin/bash
+
+# echo $BASH_VERSION
+
+# echo "lol"
+
+for i in 1 2 3 4 5; do
+    echo "the current i is $i"
+done
+
+for i in {1..10}; do
+    echo "the 2nd loop and i is $i"
+done
+
+for i in {0..10..2}; do
+    echo "even numbers and current i is $i"
+done
+
+for ((i = 0; i < 4; ++i)); do
+    echo "the 4th version and current i is $i"
+done
--- forLoopExample.sh
+++ forLoopExample.sh
@@ -1,1 +1,16 @@
 #! /usr/bin/bash
+
+for command in ls pwd date; do
+    echo "the namd of the command is $command"
+    $command
+    for i in {1..3}; do
+        echo
+    done
+done
+
+for current in *; do
+    if [ -d "$current" ]; then # will give us some errors if the name of directory contains spaces
+        echo $current
+
+    fi
+done
