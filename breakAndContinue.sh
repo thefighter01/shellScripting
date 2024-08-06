97011ec5aaadbbe693a2166c43d898ca66ba6172
thefighter01 <mahmoudbubg1234@gmail.com>
2024-08-06 08:10:12 +0300

    doYourShit

--- breakAndContinue.sh
+++ breakAndContinue.sh
@@ -1,1 +1,16 @@
 #! /usr/bin/bash
+
+for ((i = 0; i <= 10; ++i)); do
+    if [ $i -gt 5 ]; then
+        break
+    fi
+    echo $i
+done
+
+for ((i = 0; i <= 10; ++i)); do
+    if [ $i -eq 3 -o $i -eq 6 ]; then
+        continue
+    fi
+
+    echo "current i is $i"
+done
--- selectLoop.sh
+++ selectLoop.sh
@@ -1,1 +1,22 @@
 #! /usr/bin/bash
+
+# select name in mona ma7moud menna; do
+#     echo $name
+# done
+
+select name in nada ma7moud menna; do
+    case $name in
+    mark)
+        echo "mark is gay"
+        ;;
+    menna)
+        echo "menna is such an idiot persion"
+        ;;
+    ma7moud)
+        echo "here is out hero"
+        ;;
+    *)
+        echo "enter some useful shit "
+        ;;
+    esac
+done
