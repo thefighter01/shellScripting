097dbdb6cec4368ef9bf728598ecc5b6083c2dbf
thefighter01 <mahmoudbubg1234@gmail.com>
2024-08-07 09:42:22 +0300

    just a few days

--- functions.sh
+++ functions.sh
@@ -1,1 +1,16 @@
 #! /usr/bin/bash
+
+function hello() {
+    echo "helloWorld"
+    echo "the first argument $1 and 2nd one is $2 and 3rd one is $3"
+}
+
+close() {
+    exit
+}
+
+hello
+
+hello "ma7moud" "hasan" "ahmed"
+close
+echo "foo"
--- localVariable.sh
+++ localVariable.sh
@@ -1,1 +1,29 @@
 #! /usr/bin/bash
+
+function print() {
+    local name=$1 # this variable will only belong to this method
+    echo "the name is $name"
+}
+
+name="Tom"
+
+echo "before the function is called $name"
+
+print "ma7moud"
+
+echo "afer the function is called $name"
+
+echo "######################################################"
+
+function print() {
+    name=$1
+    echo "the name is $name"
+}
+
+name="Tom"
+
+echo "before the function is called $name"
+
+print "ma7moud"
+
+echo "afer the function is called $name"
