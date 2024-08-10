520e4c662eaffad339e9b4477db7500c9fdd03ab
thefighter01 <mahmoudbubg1234@gmail.com>
2024-08-10 21:05:52 +0300

    oneStepAway

--- functionsExample.sh
+++ functionsExample.sh
@@ -1,1 +1,25 @@
 #! /usr/bin/bash
+
+usage() {
+    echo "You need to provide an argument : "
+    echo "usage : $0 file_name"
+}
+
+is_file_exist() {
+    local file="$1"
+
+    [[ -f "$file" ]] && return 0 || return 1
+}
+
+function work() {
+    echo "i am comming for everybody"
+}
+[[ $# -eq 0 ]] && usage
+
+output=$(work)
+if (is_file_exist "$1"); then
+
+    echo "file exist $output lol"
+else
+    echo "file is not exist"
+fi
--- readOnly.sh
+++ readOnly.sh
@@ -1,1 +1,14 @@
 #! /usr/bin/bash
+readonly temp
+
+echo $temp
+
+function hello() {
+    echo "hello World"
+}
+readonly -f hello
+hello
+
+readonly -f
+
+readonly -p
